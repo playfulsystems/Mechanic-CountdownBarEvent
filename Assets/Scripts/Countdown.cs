3e07f6f032b23c60cc928e6e692a7dba6d7e0ca4
Jonah Warren <jonah@playfulsystems.com>
2020-07-26 15:16:52 -0400

    Initial commit

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    // event
+    public static event Action<int> TimesUp;
+
+    public float countdownEverySeconds = 1f;
+    public float counter;
+
+    void Start()
+    {
+        // init the counter
+        counter = countdownEverySeconds;
+    }
+
+    void Update()
+    {
+        // if countdown is up
+        if (counter < 0)
+		{
+            TimesUp(0); // call the delegate
+            counter = countdownEverySeconds;
+        }
+
+        // update width
+        float percentDone = counter/countdownEverySeconds;
+        transform.localScale = new Vector2(percentDone, 1f);
+
+        // update counter 
+        counter -= Time.deltaTime;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mechanic-CountdownBarEvent
+ A countdown bar that fires an event. In this example, when the time's up the player jumps.
